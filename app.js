46cd4490d6f84e84ae4ff2146b22f8d3de38321b
oliver hodge <oliverhodge2@ufl.edu>
2020-03-12 09:42:41 -0400

    edge cases

--- app.js
+++ app.js
@@ -1,37 +1,43 @@
 'use strict';
+
 const { Client } = require('pg');
 const express = require('express');
-
+const PORT = 8080;
 const app = express();
 
 app.get('/', (req, res) => {
-  if(req.query.sqlquery){
-  let client = new Client({
-    user: 'postgres',
-    host: '34.74.147.158',
-    database: 'postgres',
-    password: 'password'
-  });
-  client.connect();
-  client
-    .query(req.query.sqlquery)
-    .then(data => {
-      res
-        .status(200)
-        .send(data.rows)
-        .end();
-      client.end();
-    })
-    .catch(err => {
-      console.log(err);
-      client.end();
+  if (req.query.sqlquery) {
+    const client = new Client({
+      user: 'postgres',
+      host: '34.74.147.158',
+      database: 'postgres',
+      password: 'password'
     });
-    } else {
-    res.status(200).send('no query').end();
-    }
+    client.connect();
+    client
+      .query(req.query.sqlquery)
+      .then(data => {
+        res
+          .status(200)
+          .send(data.rows)
+          .end();
+        client.end();
+      })
+      .catch(err => {
+        console.log(err);
+        res
+          .status(200)
+          .send('bad query')
+          .end();
+        client.end();
+      });
+  } else {
+    res
+      .status(200)
+      .send('no query')
+      .end();
+  }
 });
-
-const PORT = 8080;
 
 app.listen(PORT, () => {
   console.log('App listening on port', PORT);
